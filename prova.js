3c014a9882a7a76bc9a8c48adc34e03369ea2589
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-03-21 08:54:45 -0300

    Add files via upload

---
+++ prova.js
@@ -0,0 +1,9 @@
+//INFORMAÇÕES
+let idade = 18
+let possuiPassaporte = true
+
+if (idade >= 18 && possuiPassaporte){
+    console.log ('Você pode viajar sozinho!!!');
+} else {
+    console.log ('Você não pode viajar sozinho!!!');
+}
---
+++ prova2.js
@@ -0,0 +1,22 @@
+//Lista de músicas
+let musicas = [
+    'Exile - Taylor Swift',
+    'Cardigan - Taylor Swift',
+    'August - Taylor Swift',
+    'Betty - Taylor Swift',
+    'Mirrorball - Taylor Swift',
+    'Lover - Taylor Swift',
+    'Love Story - Taylor Swift',
+    'Cruel Summer - Taylor Swift',
+    'Karma - Taylor Swift',
+    'Long Live - Taylor Swift',
+    'Dont Blame - Taylor Swift', 
+    'London Boy - Taylor Swift',
+    'Champagne Problems - Taylor Swift',
+    'All To Well - Tylor Swift'
+]
+//Selecionar música aleatória
+
+        let indiceAleatorio = Math.floor(Math.random() * 14);
+        
+console.log ('A música selecionada é :', musicas [indiceAleatorio])
